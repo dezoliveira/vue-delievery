b391e28ed962b8e2733a0c5cf0202dee6a16a6e6
dezoliveira <andresoliveira@protonmail.com>
2023-06-14 08:41:01 -0300

    Hero component

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,13 @@
 <template>
-  
+  <Hero />  
 </template>
 
 <script>
+import Hero from './components/Hero.vue'
 export default {
   name: 'App',
   components: {
-
+    Hero
   }
 }
 </script>
---
+++ src/components/Hero.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="container">
+    <div>
+      <img src="../assets/images/logo.jpg"/>
+      <h2>CHICKEN SC</h2>
+      <span class="city">
+        <i></i>
+        <h4>SÃO JOSÉ DO RIO PARDO - SP</h4>
+      </span>
+    </div>
+    <div class="row">
+      <span class="status">FECHADO</span>
+      <span>RETIRAR | ENTREGAR</span>
+      <span class="time">00:10:00</span>
+    </div>
+  </div>
+
+</template>
+<script>
+export default {
+  
+}
+</script>
+<style>
+  .container {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+  }
+
+  .container .row {
+    display: flex;
+    gap: 15px;
+  }
+
+  .container img {
+    width: 120px;
+  }
+</style>
