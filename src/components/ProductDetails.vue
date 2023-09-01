115a46999c114c8e2efa89dd88200b78108b4aa4
Andres Oliveira <andresoliveira@protonmail.com>
2023-08-31 21:20:36 -0300

    feat: Product Details Component
    - Added Product Details Component

---
+++ src/components/ProductDetails.vue
@@ -0,0 +1,119 @@
+<template>
+  <div class="container">
+    <span class="product-img">
+      <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.portaldofranchising.com.br%2Fwp-content%2Fuploads%2F2017%2F10%2Fkfc.jpg&f=1&nofb=1&ipt=83571c81ece60eae4693cd0d86d267a339ae59a8f12d19913250d57a31342cbc&ipo=images" />
+    </span>
+    <Tray>
+      <!-- <form class="form" @submit.prevent="handleSubmit(id)"> -->
+      <div class="form">
+        <h1><strong>Chicken Especial</strong></h1>
+        <p>Balde de galinha com tempero de alecrim especial sabor cebola caramelizada.</p>
+        <div class="values">
+          <span class="quantity">
+            <button class="minus" :disabled="quantity <= 1" @click="quantity--">
+              <fa icon="minus-circle"></fa>
+            </button>
+            <label :v-model="quantity">{{ quantity }}</label>
+            <button class="plus" :disabled="quantity >= 100" @click="quantity++">
+              <fa icon="plus-circle"></fa>
+            </button>
+          </span>
+          <span>
+            <h4>16.00</h4>
+          </span>
+        </div>
+        <button class="btn-add" @click="handleSubmit(id)">Adicionar</button>
+      </div>
+      <!-- </form> -->
+    </Tray>
+  </div>
+</template>
+
+<script>
+import Tray from './Tray.vue';
+export default {
+  name: 'ProductDetails',
+  components: {
+    Tray
+  },
+
+  props: ['id'],
+  data() {
+    return {
+      quantity: 1,
+    }
+  },
+
+  methods: {
+    handleSubmit(id){
+      alert(id)
+    },
+  }
+}
+</script>
+
+<style scoped>
+  .container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: flex-start;
+    /* gap: 10px; */
+    height: 100%;
+    padding: 18px;
+  }
+
+  .container .product-img {
+    width: 300px;
+  }
+
+  .container .product-img img{
+    width: 100%;
+    border-radius: 15px
+  }
+
+  .form {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 20px;
+  }
+
+  .form p {
+    text-align: left;
+  }
+
+  .form .values {
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  .form .values .quantity {
+    display: flex;
+    align-items: center;
+    gap: 10px;
+  }
+
+  .form .values .quantity button {
+    font-size: 25px;
+  }
+
+  .form .values .quantity button.plus {
+    color: #16a34a;
+
+    /* firefox */
+    border: 0;
+    background: none;
+  }
+
+  .form .values .quantity button.minus {
+    color: #dc2626;
+
+    /* firefox */
+    border: 0;
+    background: none;
+  }
+</style>
--- src/main.js
+++ src/main.js
@@ -4,11 +4,11 @@ import '../src/main.css'
 
 //Font Awesome
 import { Library, library } from '@fortawesome/fontawesome-svg-core'
-import { faChevronRight, faChevronDown, faClock, faLocationDot, faClose, faMotorcycle, faPersonWalking, faArrowLeft } from '@fortawesome/free-solid-svg-icons'
+import { faChevronRight, faChevronDown, faClock, faLocationDot, faClose, faMotorcycle, faPersonWalking, faArrowLeft, faPlusCircle, faMinusCircle } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import router from './router'
 
-library.add(faChevronRight, faChevronDown, faLocationDot, faClock, faClose, faMotorcycle, faPersonWalking, faArrowLeft)
+library.add(faChevronRight, faChevronDown, faLocationDot, faClock, faClose, faMotorcycle, faPersonWalking, faArrowLeft, faPlusCircle, faMinusCircle)
 
 createApp(App).use(router)
   .component("fa", FontAwesomeIcon)
